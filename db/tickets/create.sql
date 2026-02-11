4725d34543bc42ece356e955593a5d2a7978a2a1
Eldar Dzhorobekov <eldar.dzhorobekov@gmail.com>
2026-02-11 07:27:52 +0700

    update gitignore

--- db/tickets/create.sql
+++ db/tickets/create.sql
@@ -2,12 +2,14 @@ CREATE TABLE IF NOT EXISTS flass.tickets (
     id SERIAL PRIMARY KEY,
     chat_id TEXT NOT NULL,
     chat_name TEXT NOT NULL,
+    message_id TEXT NOT NULL,
     route_from TEXT NOT NULL,
+    posted_at timestamp with time zone,
     route_to TEXT NOT NULL,
-    date_start TIMESTAMP NOT NULL,
-    date_end TIMESTAMP,
+    date_start timestamp with time zone NOT NULL,
+    date_end timestamp with time zone,
     price FLOAT NOT NULL,
     currency TEXT NOT NULL,
     airline TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP
 );
